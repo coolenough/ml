241b42a5c9bd2f53bcd4a4cfbecf1770483eeec4
coolenough <25je0892@iitism.ac.in>
2025-12-16 18:41:32 +0530

    Added Optimizers and Weight Initializers and a new nonlin func

--- files/nn.py
+++ files/nn.py
@@ -1,5 +1,5 @@
 class Neuron:
-    def __init__(self, data,children = ()):
+    def __init__(self, data,children = (),_backward = lambda : None):
         self.data = data
         self.grad = 0
         self.children = children
@@ -40,14 +40,17 @@ class Neuron:
         return out
     def __truediv__(self,other):
         other = other if isinstance(other,Neuron) else Neuron(other)
+        def _backward():
+            self.grad += out.grad/other.data
+            other.grad += -(self.data / (other.data ** 2)) * out.grad
         try:
             out = self.data/other.data
-            return Neuron(out)
+            return Neuron(out , children = (self,other),_backward = _backward)
         except ZeroDivisionError:
             out = Neuron(0)
         def _backward():
             self.grad += out.grad/other.data
-            other.grad += -(self.data/((other.data)**2))*out.grad
+            other.grad += -(self.data / (other.data ** 2)) * out.grad
         out.children = (self,other)
         out._backward = _backward
         return out
@@ -72,7 +75,7 @@ class Neuron:
         return out
     def __rsub__(self,other):
         other = other if isinstance(other,Neuron) else Neuron(other)
-        out = (self.data - other.data)
+        out = Neuron(self.data - other.data)
         out.children = (self,other)
         def _backward():
             self.grad += 1.0*out.grad
@@ -90,14 +93,14 @@ class Neuron:
         return out
     def __rtruediv__(self,other):
         other = other if isinstance(other,Neuron) else Neuron(other)
-        try:
-            out = self.data/other.data
-            return Neuron(out)
-        except ZeroDivisionError:
-            out = Neuron(0)
         def _backward():
             self.grad += out.grad/other.data
-            other.grad += -(self.data/((other.data)**2))*out.grad
+            other.grad += -(self.data / (other.data ** 2)) * out.grad
+        try:
+            out = self.data/other.data
+            return Neuron(out,children = (self,other),_backward = _backward)
+        except ZeroDivisionError:
+            out = Neuron(0)
         out.children = (self,other)
         out._backward = _backward
         return out
@@ -113,6 +116,13 @@ class Neuron:
         out.children = (self,)
         def _backward():
             self.grad += -1.0*out.grad
+        out._backward = _backward
+        return out
+    def tanh(self):
+        out = Neuron((np.exp(2*self.data) - 1)/(np.exp(2*self.data) + 1))
+        out.children = (self,)
+        def _backward():
+            self.grad += out.grad*(1 - out.data ** 2)
         out._backward = _backward
         return out
     def __repr__(self):
@@ -142,8 +152,10 @@ class Module:
 class Node(Module):
 
     def __init__(self, nin, nonlin=True):
-        self.w = [Neuron(random.uniform(-1,1)) for _ in range(nin)]
+        # intializing the default mode be a relu unit with He activation
+        self.w = [Neuron(np.random.normal(0,np.sqrt(2/nin))) for _ in range(nin)]
         self.b = Neuron(0)
+        self.nin = nin
         self.nonlin = nonlin
 
     def __call__(self, x):
@@ -158,8 +170,20 @@ class Node(Module):
 
 class Layer(Module):
 
-    def __init__(self, nin, nout, **kwargs):
-        self.neurons = [Node(nin, **kwargs) for _ in range(nout)]
+    def __init__(self, nin, nout,weight_intializer = 'He' ,nonlin = False ,**kwargs):
+        self.neurons = [Node(nin, nonlin) for _ in range(nout)]
+        def He():
+            for neuron in self.neurons:
+                neuron.w = [Neuron(np.random.normal(0, np.sqrt(2 / neuron.nin))) for _ in range(neuron.nin)]
+        def Xavier():
+            for neuron in self.neurons:
+                neuron.w = [Neuron(np.random.normal(0, np.sqrt(2 / (neuron.nin + nout)))) for _ in range(neuron.nin)]
+        if weight_intializer == 'He':
+            He()
+        elif weight_intializer == 'Xavier':
+            Xavier()
+        else:
+            raise ValueError(f'Unknown weight initialization : {weight_intializer}')
 
     def __call__(self, x):
         out = [n(x) for n in self.neurons]
@@ -173,9 +197,10 @@ class Layer(Module):
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
-        sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+    def __init__(self, Layers):
+        self.x = None
+        self.y = None
+        self.layers = Layers
 
     def __call__(self, x):
         for layer in self.layers:
@@ -188,9 +213,55 @@ class MLP(Module):
     def __repr__(self):
         return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
 
+    def fit(self,x,y,optimizer = 'adam',epochs = 1000):
+        self.x = x
+        self.y = y
+        self.optimizer = optimizer
+        y_pred = [self(xs) for xs in self.x]
+        cost = sum((yout - ygt) ** 2 for ygt, yout in zip(self.y, y_pred))
+        self.m = [0]*len(self.parameters())
+        self.v = [0]*len(self.parameters())
+        self.epoch = 1
+        self.epoch_limit = epochs
 
+        def adam(learning_rate = 0.001):
+            self.m =[0.9*w1 + 0.1*grad for w1,grad in zip(self.m, [p.grad for p in self.parameters()])]
+            self.v =[0.999*v1 + 0.001*(grad**2) for v1,grad in zip(self.v, [p.grad for p in self.parameters()])]
+            hat_m = [_m / (1 - 0.9 ** self.epoch) for _m in self.m]
+            hat_v = [_v / (1 - 0.999 ** self.epoch) for _v in self.v]
+            r_v = [np.sqrt(1/(_hat_v + 0.1**4)) for _hat_v in hat_v]
+            for params,_hat_m,_r_v in zip(self.parameters(),hat_m,r_v):
+                params.data -= learning_rate*_hat_m*_r_v
 
+        def RMSprop(learning_rate = 0.001):
+            self.v =[0.999*v1 + 0.001*(grad**2) for v1,grad in zip(self.v, [p.grad for p in self.parameters()])]
+            r_v = [(1/(np.sqrt(_hat_v) + 0.1**4)) for _hat_v in self.v]
+            for params,_r_v in zip(self.parameters(),r_v):
+                params.data -= learning_rate*_r_v*params.grad
 
+        def momentum(learning_rate = 0.001):
+            self.m =[0.9*w1 + 0.1*grad for w1,grad in zip(self.m, [p.grad for p in self.parameters()])]
+            for params,_hat_m in zip(self.parameters(),self.m):
+                params.data -= learning_rate*_hat_m
+
+        if self.optimizer == 'adam':
+            update_step = adam
+        elif self.optimizer =='RMSprop':
+            update_step = RMSprop
+        elif self.optimizer =='momentum':
+            update_step = momentum
+        else:
+            raise ValueError(f'Unknown Optimizer : {self.optimizer}')
+
+        while self.epoch <= self.epoch_limit:
+            self.zero_grad()
+            y_pred = [self(xs) for xs in self.x]
+            cost = sum((yout - ygt) ** 2 for ygt, yout in zip(self.y, y_pred))
+            cost.backward()
+            update_step()
+            if self.epoch%50 == 0:
+                print(f"Epoch {self.epoch} | Loss: {cost.data:.4f}")
+            self.epoch += 1
 
 
 
